eef15ca714119e708c38a641d501e38a7b3d88c3
Vitaliy <vitaliy0187@gmail.com>
2020-06-20 14:43:50 +0300

    Add js directory

---
+++ JavaScript/js/hw02.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const total = 100;
+let ordered;
+ 
+ordered = 50;
+
+if (ordered <= total) {
+  console.log("Заказ оформлен, с вами свяжется менеджер".);
+}else {
+    console.log("На складе недостаточно твоаров!".);
+    
+}
---
+++ JavaScript/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let a = Number("56");
+let b = 35;
+
+console.log(Number.isNaN(a));
