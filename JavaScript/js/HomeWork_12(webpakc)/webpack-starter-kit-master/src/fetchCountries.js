ea7cdc8c49d76c3cea6917b2a1b2e6790904ef18
Vitaliy <vitaliy0187@gmail.com>
2020-11-15 15:11:32 +0300

    add new js file

---
+++ JavaScript/js/HomeWork_12(webpakc)/webpack-starter-kit-master/src/fetchCountries.js
@@ -0,0 +1,10 @@
+const url = "https://restcountries.eu/rest/v2/name/"
+
+
+export default function fetchCountries(searchQuery){ 
+    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`
+  
+    return fetch(url).then(response => response.json())
+  }
+
+
---
+++ JavaScript/js/HomeWork_12(webpakc)/webpack-starter-kit-master/src/renderCountries.js
@@ -0,0 +1,23 @@
+import tamplate from './tamplates/tamplate.hbs';
+
+const listCountries = document.querySelector(".countries-list")
+
+function renderCountries(countries){
+console.log("renderCountries -> countries", countries)
+
+    if(countries.length > 1){
+    clearForm();
+    countries.forEach(country => listCountries.insertAdjacentHTML("afterbegin", `<li class="list__name">${country.name}</li>`))
+    }else{
+    clearForm();
+
+     const murkup = tamplate(countries);
+    listCountries.insertAdjacentHTML("afterbegin", murkup)
+    }
+}
+
+function clearForm(){
+    listCountries.innerHTML = "";
+}
+
+export {listCountries, renderCountries};
