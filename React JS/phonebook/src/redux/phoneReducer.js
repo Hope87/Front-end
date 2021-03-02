ad9d2d5b395e43b94f430c473edb833215ff98fe
Vitaliy <vitaliy0187@gmail.com>
2021-03-02 11:25:27 +0300

    add new project

--- React JS/phonebook/src/redux/phoneReducer.js
+++ React JS/phonebook/src/redux/phoneReducer.js
@@ -1,4 +1,4 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers } from "redux";
 import { createReducer } from "@reduxjs/toolkit";
 import * as phoneActions from "./phoneActions";
 
